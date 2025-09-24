3a13cce0a51186929fe9449ee7f48ec656fa0433
nazuratama <nazurawirayudatama7@gmail.com>
2025-09-24 00:10:58 +0000

    problems 17 python with 0 ms

--- problems/python/17. Letter Combinations of a Phone Number.py
+++ problems/python/17. Letter Combinations of a Phone Number.py
@@ -1,1 +1,26 @@
-cek saja
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+
+        dmap = {
+            "2":"abc", 
+            "3":"def", 
+            "4":"ghi", 
+            "5":"jkl", 
+            "6":"mno", 
+            "7":"pqrs", 
+            "8":"tuv", 
+            "9":"wxyz"}
+        
+        q = [""]
+
+        for d in digits:
+            n = len(q)
+            for _ in range(n):
+                path = q.pop(0)
+                ltrs = dmap[d]
+                for c in ltrs:
+                    q.append(path + c)
+        
+        return q
