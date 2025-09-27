4f5fa995bb4d96ad881afc61e430c79ac8c11681
nazuratama <nazurawirayudatama7@gmail.com>
2025-09-27 09:06:05 +0700

    problems 23 python with 8 ms

---
+++ problems/python/23. Merge k Sorted Lists.py
@@ -0,0 +1,36 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists:
+            return None
+        
+        while len(lists) > 1:
+            mres = []
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i + 1] if (i + 1) < len(lists) else None
+                mres.append(self.m2l(l1, l2))
+            lists = mres
+            
+        return lists[0]
+
+    def m2l(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dumm = ListNode()
+        curr = dumm
+        
+        while l1 and l2:
+            if l1.val < l2.val:
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+            curr = curr.next
+        
+        curr.next = l1 or l2
+        return dumm.next
