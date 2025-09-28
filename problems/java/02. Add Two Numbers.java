7c3065af348f0ca8003e8aac18ff0835016751a3
nazuratama <nazurawirayudatama7@gmail.com>
2025-09-28 12:56:33 +0700

    problems 2 java with 1 ms

---
+++ problems/java/02. Add Two Numbers.java
@@ -0,0 +1,34 @@
+// class ListNode {
+//     int val;
+//     ListNode next;
+//     ListNode() {}
+//     ListNode(int val) { this.val = val; }
+//     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+// }
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode curr = head;
+        int c = 0;
+
+        while (l1 != null || l2 != null || c != 0) {
+            int v1 = 0;
+            int v2 = 0;
+            if (l1 != null) {
+                v1 = l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                v2 = l2.val;
+                l2 = l2.next;
+            }
+            
+            int sum = v1 + v2 + c;
+            c = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+        }
+        return head.next;
+    }
+}
