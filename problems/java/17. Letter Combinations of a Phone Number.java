5407b8eb038cca800a649ebf5022d27b7ffac6d9
nazuratama <nazurawirayudatama7@gmail.com>
2025-09-28 17:03:47 +0700

    problems 17 java with 6 ms - update code

--- problems/java/17. Letter Combinations of a Phone Number.java
+++ problems/java/17. Letter Combinations of a Phone Number.java
@@ -5,8 +5,13 @@ class Solution {
         }
         
         Map<Character, String> dmap = Map.of(
-            '2', "abc", '3', "def", '4', "ghi", '5', "jkl",
-            '6', "mno", '7', "pqrs", '8', "tuv", '9', "wxyz"
+            '2', "abc", 
+            '3', "def", 
+            '4', "ghi", 
+            '5', "jkl",
+            '6', "mno", '7', "pqrs", 
+            '8', "tuv", 
+            '9', "wxyz"
         );
         
         LinkedList<String> q = new LinkedList<>();
