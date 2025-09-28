4d1f21ea257fc3c73c10b0092c26e4a6c85bc197
nazuratama <nazurawirayudatama7@gmail.com>
2025-09-28 16:46:47 +0700

    problems 16 java with 13 ms

---
+++ problems/java/16. 3Sum Closest.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int res = nums[0] + nums[1] + nums[2];
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            int l = i + 1;
+            int r = nums.length - 1;
+            while (l < r) {
+                int csum = nums[i] + nums[l] + nums[r];
+                if (Math.abs(csum - target) < Math.abs(res - target)) {
+                    res = csum;
+                }
+                
+                if (csum < target) {
+                    l++;
+                } else if (csum > target) {
+                    r--;
+                } else {
+                    return csum;
+                }
+            }
+        }
+        return res;
+    }
+}
