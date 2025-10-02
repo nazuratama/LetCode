ab1c2d9f224df63a5f7ab1bd198ea49a828a4787
nazuratama <nazurawirayudatama7@gmail.com>
2025-10-02 14:41:58 +0700

    problems 5 c++ with 5 ms

---
+++ problems/C++/5. Longest Palindromic Substring.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        if (s.empty()) {
+            return "";
+        }
+        int st = 0, en = 0;
+
+        for (int i = 0; i < s.length(); ++i) {
+            int len1 = expd(s, i, i);
+            int len2 = expd(s, i, i + 1);
+            int mlen = std::max(len1, len2);
+            if (mlen > en - st) {
+                st = i - (mlen - 1) / 2;
+                en = i + mlen / 2;
+            }
+        }
+        return s.substr(st, en - st + 1);
+    }
+
+private:
+    int expd(const std::string& s, int l, int r) {
+        while (l >= 0 && r < s.length() && s[l] == s[r]) {
+            l--;
+            r++;
+        }
+        return r - l - 1;
+    }
+};
