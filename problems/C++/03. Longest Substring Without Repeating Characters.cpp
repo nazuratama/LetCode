df6cdd236511c84265b4c1f393e374515e8ecaa1
nazuratama <nazurawirayudatama7@gmail.com>
2025-10-02 07:45:39 +0700

    problems 3 c++ with 3 ms

---
+++ problems/C++/03. Longest Substring Without Repeating Characters.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        
+        unordered_map<char, int> map;
+        int res = 0;
+        int l = 0;
+
+        for (int r = 0; r < s.length(); ++r) {
+            char c = s[r];
+            if (map.count(c) && map[c] >= l) {
+                l = map[c] + 1;
+            }
+            map[c] = r;
+            res = std::max(res, r - l + 1);
+        }
+        return res;
+    }
+};
