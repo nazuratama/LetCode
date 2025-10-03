f4c6df4a570550e731ece7793f6b365d360f33ff
nazuratama <nazurawirayudatama7@gmail.com>
2025-10-03 18:35:36 +0700

    problems 9 c++ with 0 ms

---
+++ problems/C++/9. Palindrome Number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int revh = 0;
+        while (x > revh) {
+            revh = revh * 10 + x % 10;
+            x /= 10;
+        }
+
+        return x == revh || x == revh / 10;
+    }
+};
