e800dcb2dc63a641e5d9c5b32195f7b0e324758b
nazuratama <nazurawirayudatama7@gmail.com>
2025-10-04 17:37:20 +0700

    problems 31 c++ with 0 ms

---
+++ problems/C++/31. Next Permutation.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int i = n - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        if (i >= 0) {
+            int j = n - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums[i], nums[j]);
+        }
+
+        rev(nums, i + 1, n - 1);
+    }
+private:
+    void rev(vector<int>& nums, int l, int r) {
+        while (l < r) {
+            swap(nums[l], nums[r]);
+            l++;
+            r--;
+        }
+    }
+};
