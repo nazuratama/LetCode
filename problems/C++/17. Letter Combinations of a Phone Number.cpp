ec4ae536945e6d42aa261db788fa0fdf269b8f0c
nazuratama <nazurawirayudatama7@gmail.com>
2025-10-04 07:59:47 +0700

    rename file c++ & solved problems 17 c++ with 0 ms

---
+++ problems/C++/17. Letter Combinations of a Phone Number.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        if (digits.empty()) {
+            return {};
+        }
+
+        unordered_map<char, string> dmap = {
+            {'2', "abc"}, 
+            {'3', "def"}, 
+            {'4', "ghi"}, 
+            {'5', "jkl"},
+            {'6', "mno"}, 
+            {'7', "pqrs"}, 
+            {'8', "tuv"}, 
+            {'9', "wxyz"}
+        };
+        
+        vector<string> res;
+        res.push_back("");
+
+        for (char d : digits) {
+            vector<string> temp;
+            for (const string& path : res) {
+                for (char c : dmap[d]) {
+                    temp.push_back(path + c);
+                }
+            }
+            res = temp;
+        }
+        return res;
+    }
+};
