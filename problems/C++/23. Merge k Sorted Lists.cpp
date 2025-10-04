c260f9730102b8cc7c28fbe2b90bc9c76c0a6c0b
nazuratama <nazurawirayudatama7@gmail.com>
2025-10-04 10:58:34 +0700

    problems 23 c++ with 3 ms

---
+++ problems/C++/23. Merge k Sorted Lists.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if (lists.empty()) {
+            return nullptr;
+        }
+        while (lists.size() > 1) {
+            vector<ListNode*> mres;
+            for (int i = 0; i < lists.size(); i += 2) {
+                ListNode* l1 = lists[i];
+                ListNode* l2 = (i + 1 < lists.size()) ? lists[i + 1] : nullptr;
+                mres.push_back(m2l(l1, l2));
+            }
+            lists = mres;
+        }
+        return lists[0];
+    }
+
+private:
+    ListNode* m2l(ListNode* l1, ListNode* l2) {
+        ListNode dumm(0);
+        ListNode* curr = &dumm;
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                curr->next = l1;
+                l1 = l1->next;
+            } else {
+                curr->next = l2;
+                l2 = l2->next;
+            }
+            curr = curr->next;
+        }
+        curr->next = l1 ? l1 : l2;
+        return dumm.next;
+    }
+};
