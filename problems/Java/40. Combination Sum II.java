957a42213c056401f8f429f8afe8e4c832564f6a
nazuratama <nazurawirayudatama7@gmail.com>
2025-10-10 14:51:09 +0700

    problems 40 java with 5 ms

--- problems/Java/40. Combination Sum II.java
+++ problems/Java/40. Combination Sum II.java
@@ -1,12 +1,12 @@
 class Solution {
-    public List<List<Integer>> combinationSum2(int[] cand, int targ) {
-        Arrays.sort(cand);
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
         List<List<Integer>> res = new ArrayList<>();
-        bt(cand, targ, new ArrayList<>(), 0, res);
+        bt(candidates, target, new ArrayList<>(), 0, res);
         return res;
     }
 
-    private void bt(int[] cand, int rem, List<Integer> path, int start, List<List<Integer>> res) {
+    private void bt(int[] candidates, int rem, List<Integer> path, int start, List<List<Integer>> res) {
         if (rem == 0) {
             res.add(new ArrayList<>(path));
             return;
@@ -15,12 +15,12 @@ class Solution {
             return;
         }
 
-        for (int i = start; i < cand.length; i++) {
-            if (i > start && cand[i] == cand[i-1]) {
+        for (int i = start; i < candidates.length; i++) {
+            if (i > start && candidates[i] == candidates[i-1]) {
                 continue;
             }
-            path.add(cand[i]);
-            bt(cand, rem - cand[i], path, i + 1, res);
+            path.add(candidates[i]);
+            bt(candidates, rem - candidates[i], path, i + 1, res);
             path.remove(path.size() - 1);
         }
     }
