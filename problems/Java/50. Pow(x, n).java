f26460433bae0ea3592db4f4afbcec0c12ba5390
nazuratama <nazurawirayudatama7@gmail.com>
2025-10-20 06:29:18 +0700

    problems 50 java with 0 ms

---
+++ problems/Java/50. Pow(x, n).java
@@ -0,0 +1,21 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        
+        double res = 1.0;
+        
+        while (N > 0) {
+            if (N % 2 == 1) {
+                res *= x;
+            }
+            x *= x;
+            N /= 2;
+        }
+        
+        return res;
+    }
+}
