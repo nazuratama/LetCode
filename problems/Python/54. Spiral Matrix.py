a2bb5aa26a574f9a560a26794afa11627f72839d
nazuratama <nazurawirayudatama7@gmail.com>
2025-10-24 07:10:55 +0700

    problems 54 python with 0 ms

---
+++ problems/Python/54. Spiral Matrix.py
@@ -0,0 +1,30 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        if not matrix:
+            return []
+        
+        res = []
+        l, r = 0, len(matrix[0]) - 1
+        t, b = 0, len(matrix) - 1
+        
+        while l <= r and t <= b:
+            for i in range(l, r + 1):
+                res.append(matrix[t][i])
+            t += 1
+            
+            for i in range(t, b + 1):
+                res.append(matrix[i][r])
+            r -= 1
+            
+            if not (l <= r and t <= b):
+                break
+            
+            for i in range(r, l - 1, -1):
+                res.append(matrix[b][i])
+            b -= 1
+                
+            for i in range(b, t - 1, -1):
+                res.append(matrix[i][l])
+            l += 1
+            
+        return res
