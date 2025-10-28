70656802b4c7b9d0c9ec773e27881e199605f010
nazuratama <nazurawirayudatama7@gmail.com>
2025-10-28 07:16:10 +0700

    problems 58 python with 0 ms

--- problems/Python/58. Length of Last Word.py
+++ problems/Python/58. Length of Last Word.py
@@ -1,9 +1,10 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        len = 0
+        count = 0
         for i in range(len(s) - 1, -1, -1):
-            if s[i] != ' ':
-                len += 1
-            elif len > 0:
-                return len
-        return len
+            if s[i] == ' ':
+                if count > 0:
+                    return count
+            else:
+                count += 1
+        return count
