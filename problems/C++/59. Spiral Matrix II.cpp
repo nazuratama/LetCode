a4e7c0e38873d4879680a160f93a0d31df5284da
nazuratama <nazurawirayudatama7@gmail.com>
2025-10-29 12:45:32 +0700

    problems 59 c++ with 0 ms

---
+++ problems/C++/59. Spiral Matrix II.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        vector<vector<int>> mat(n, vector<int>(n));
+        int l = 0, r = n - 1;
+        int t = 0, b = n - 1;
+        int num = 1;
+
+        while (l <= r && t <= b) {
+            for (int i = l; i <= r; ++i) {
+                mat[t][i] = num++;
+            }
+            t++;
+
+            for (int i = t; i <= b; ++i) {
+                mat[i][r] = num++;
+            }
+            r--;
+
+            for (int i = r; i >= l; --i) {
+                mat[b][i] = num++;
+            }
+            b--;
+
+            for (int i = b; i >= t; --i) {
+                mat[i][l] = num++;
+            }
+            l++;
+        }
+        return mat;
+    }
+};
