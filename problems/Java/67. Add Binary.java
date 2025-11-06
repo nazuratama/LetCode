0af32df2bbe407def6a8f0a99813be301302bcae
nazuratama <nazurawirayudatama7@gmail.com>
2025-11-06 08:16:38 +0700

    problems 67 java with 1 ms

---
+++ problems/Java/67. Add Binary.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder res = new StringBuilder();
+        int c = 0;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+
+        while (i >= 0 || j >= 0 || c > 0) {
+            if (i >= 0) {
+                c += a.charAt(i--) - '0';
+            }
+            if (j >= 0) {
+                c += b.charAt(j--) - '0';
+            }
+            res.append(c % 2);
+            c /= 2;
+        }
+
+        return res.reverse().toString();
+    }
+}
