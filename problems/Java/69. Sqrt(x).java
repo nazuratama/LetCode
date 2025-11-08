3d80847e6a98895268ed4cac064efd016a9b247b
nazuratama <nazurawirayudatama7@gmail.com>
2025-11-08 08:31:38 +0700

    problems 69 java with 1 ms

---
+++ problems/Java/69. Sqrt(x).java
@@ -0,0 +1,19 @@
+class Solution {
+    public int mySqrt(int x) {
+        if (x == 0) {
+            return 0;
+        }
+        int l = 1, r = x;
+        int res = 0;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (mid <= x / mid) {
+                res = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return res;
+    }
+}
