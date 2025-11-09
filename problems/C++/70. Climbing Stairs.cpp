2b6073aab68c51b3f928eadce32b57864a45b979
nazuratama <nazurawirayudatama7@gmail.com>
2025-11-09 10:21:40 +0700

    problems 70 c++ with 0 ms

---
+++ problems/C++/70. Climbing Stairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        
+        int one = 2;
+        int two = 1;
+        
+        for (int i = 3; i <= n; ++i) {
+            int curr = one + two;
+            two = one;
+            one = curr;
+        }
+        
+        return one;
+    }
+};
