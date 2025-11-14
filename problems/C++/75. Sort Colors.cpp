eb34cd18655d407ddacc6c8ebf778af138568efd
nazuratama <nazurawirayudatama7@gmail.com>
2025-11-14 20:44:50 +0700

    problems 75 c++ with 0 ms

---
+++ problems/C++/75. Sort Colors.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int l = 0, r = nums.size() - 1;
+        int i = 0;
+        while (i <= r) {
+            if (nums[i] == 0) {
+                swap(nums[l], nums[i]);
+                l++;
+                i++;
+            } else if (nums[i] == 2) {
+                swap(nums[r], nums[i]);
+                r--;
+            } else {
+                i++;
+            }
+        }
+    }
+};
