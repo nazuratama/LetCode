875d8503773406f1e640d91d2c4c45a90352f9fb
nazuratama <nazurawirayudatama7@gmail.com>
2025-11-21 19:21:20 +0700

    problems 82 java with 0 ms

---
+++ problems/Java/82. Remove Duplicates from Sorted List II.java
@@ -0,0 +1,25 @@
+// class ListNode {
+//     int val;
+//     ListNode next;
+//     ListNode() {}
+//     ListNode(int val) { this.val = val; }
+//     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+// }
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode dumm = new ListNode(0, head);
+        ListNode prev = dumm;
+
+        while (prev.next != null && prev.next.next != null) {
+            if (prev.next.val == prev.next.next.val) {
+                int dup_val = prev.next.val;
+                while (prev.next != null && prev.next.val == dup_val) {
+                    prev.next = prev.next.next;
+                }
+            } else {
+                prev = prev.next;
+            }
+        }
+        return dumm.next;
+    }
+}
