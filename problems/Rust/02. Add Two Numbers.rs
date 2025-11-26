7ce48bd94086d8dd9862cd1e8cbe7414d6a66aa9
nazuratama <nazurawirayudatama7@gmail.com>
2025-11-26 18:52:28 +0700

    problems 2 rust with 0 ms

--- problems/Rust/02. Add Two Numbers.rs
+++ problems/Rust/02. Add Two Numbers.rs
@@ -14,7 +14,6 @@
 //     }
 //   }
 // }
-
 impl Solution {
     pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
         let mut head = Some(Box::new(ListNode::new(0)));
