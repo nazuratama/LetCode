931b552831f9a7a1113f0e7ceb776d0469ee782a
nazuratama <nazurawirayudatama7@gmail.com>
2025-11-26 18:03:05 +0700

    problems 87 java with 8 ms

---
+++ problems/Java/87. Scramble String.java
@@ -0,0 +1,35 @@
+class Solution {
+    private Map<String, Boolean> memo = new HashMap<>();
+
+    public boolean isScramble(String s1, String s2) {
+        String key = s1 + "#" + s2;
+        if (memo.containsKey(key)) return memo.get(key);
+        if (s1.equals(s2)) return true;
+
+        char[] c1 = s1.toCharArray();
+        char[] c2 = s2.toCharArray();
+        Arrays.sort(c1);
+        Arrays.sort(c2);
+        if (!Arrays.equals(c1, c2)) return false;
+
+        int n = s1.length();
+        for (int i = 1; i < n; i++) {
+            boolean no_swap = isScramble(s1.substring(0, i), s2.substring(0, i)) &&
+                              isScramble(s1.substring(i), s2.substring(i));
+            if (no_swap) {
+                memo.put(key, true);
+                return true;
+            }
+            
+            boolean swap = isScramble(s1.substring(0, i), s2.substring(n - i)) &&
+                           isScramble(s1.substring(i), s2.substring(0, n - i));
+            if (swap) {
+                memo.put(key, true);
+                return true;
+            }
+        }
+        
+        memo.put(key, false);
+        return false;
+    }
+}
