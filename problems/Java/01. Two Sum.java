0fa45f81fdde09dbc8019b1249cb986b6c78b63d
nazuratama <nazurawirayudatama7@gmail.com>
2025-12-03 21:29:37 +0700

    problems 1 java with 0 ms

--- problems/Java/01. Two Sum.java
+++ problems/Java/01. Two Sum.java
@@ -1,12 +1,11 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            int diff = target - nums[i];
-            if (map.containsKey(diff)) {
-                return new int[] {map.get(diff), i};
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                if (nums[j] + nums[j - i] == target) {
+                    return new int[] {j - i, j};
+                }
             }
-            map.put(nums[i], i);
         }
         return new int[0];
     }
