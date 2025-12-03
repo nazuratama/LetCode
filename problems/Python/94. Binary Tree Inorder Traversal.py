98257f31c4ae1f1ee3801be229c4fc28d0715af1
nazuratama <nazurawirayudatama7@gmail.com>
2025-12-03 07:40:53 +0700

    problems 94 python with 0 ms

---
+++ problems/Python/94. Binary Tree Inorder Traversal.py
@@ -0,0 +1,22 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        stk = []
+        curr = root
+        
+        while curr or stk:
+            while curr:
+                stk.append(curr)
+                curr = curr.left
+            
+            curr = stk.pop()
+            res.append(curr.val)
+            curr = curr.right
+            
+        return res
