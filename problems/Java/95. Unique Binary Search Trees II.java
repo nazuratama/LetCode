1a5addb0a23dcf047dcd05169656df9fc1d6b9d5
nazuratama <nazurawirayudatama7@gmail.com>
2025-12-04 19:58:49 +0700

    problems 95 java with 1 ms

--- problems/Java/95. Unique Binary Search Trees II.java
+++ problems/Java/95. Unique Binary Search Trees II.java
@@ -1,16 +1,18 @@
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode() {}
-    TreeNode(int val) { this.val = val; }
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<TreeNode> generateTrees(int n) {
         if (n == 0) return new ArrayList<>();
