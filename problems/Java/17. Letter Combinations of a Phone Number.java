cbad0ecc9e564c4e4486d93f7e55c26255344a40
nazuratama <nazurawirayudatama7@gmail.com>
2025-12-05 22:16:46 +0700

    problems 17 java with 3 ms

--- problems/Java/17. Letter Combinations of a Phone Number.java
+++ problems/Java/17. Letter Combinations of a Phone Number.java
@@ -9,7 +9,8 @@ class Solution {
             '3', "def", 
             '4', "ghi", 
             '5', "jkl",
-            '6', "mno", '7', "pqrs", 
+            '6', "mno", 
+            '7', "pqrs", 
             '8', "tuv", 
             '9', "wxyz"
         );
