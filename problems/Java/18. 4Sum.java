e79cfc842c2cdef6b03ad2e291311dc04f463543
nazuratama <nazurawirayudatama7@gmail.com>
2025-12-05 22:18:28 +0700

    problems 18 java with 14 ms

--- problems/Java/18. 4Sum.java
+++ problems/Java/18. 4Sum.java
@@ -2,6 +2,7 @@ class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList<>();
+        
         int n = nums.length;
 
         for (int i = 0; i < n - 3; i++) {
