29bdd8ba9c1904eafe54d2d3d349220dda5caeee
nazuratama <nazurawirayudatama7@gmail.com>
2025-12-05 19:34:43 +0700

    problems 13 java with 2 ms

--- problems/Java/13. Roman to Integer.java
+++ problems/Java/13. Roman to Integer.java
@@ -1,19 +1,20 @@
 class Solution {
     public int romanToInt(String s) {
-        Map<Character, Integer> rmap = new HashMap<>();
-        rmap.put('I', 1);
-        rmap.put('V', 5);
-        rmap.put('X', 10);
-        rmap.put('L', 50);
-        rmap.put('C', 100);
-        rmap.put('D', 500);
-        rmap.put('M', 1000);
-        
         int res = 0;
         int prev = 0;
+        int val = 0;
         
         for (int i = s.length() - 1; i >= 0; i--) {
-            int val = rmap.get(s.charAt(i));
+            switch (s.charAt(i)) {
+                case 'I': val = 1; break;
+                case 'V': val = 5; break;
+                case 'X': val = 10; break;
+                case 'L': val = 50; break;
+                case 'C': val = 100; break;
+                case 'D': val = 500; break;
+                case 'M': val = 1000; break;
+            }
+            
             if (val < prev) {
                 res -= val;
             } else {
