89b010c0c62bb704271bc19910c7301a000a6ccc
nazuratama <nazurawirayudatama7@gmail.com>
2025-12-06 08:53:19 +0700

    problems 26 java with 0 ms

--- problems/Java/26. Remove Duplicates from Sorted Array.java
+++ problems/Java/26. Remove Duplicates from Sorted Array.java
@@ -5,6 +5,7 @@ class Solution {
         }
         
         int j = 1;
+        
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] != nums[i - 1]) {
                 nums[j] = nums[i];
