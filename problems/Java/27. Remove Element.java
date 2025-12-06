0ce29358c506049b1605b79febf767618c80024b
nazuratama <nazurawirayudatama7@gmail.com>
2025-12-06 08:54:19 +0700

    problems 27 java with 0 ms

--- problems/Java/27. Remove Element.java
+++ problems/Java/27. Remove Element.java
@@ -2,6 +2,7 @@ class Solution {
     public int removeElement(int[] nums, int val) {
         int i = 0;
         int n = nums.length;
+        
         while (i < n) {
             if (nums[i] == val) {
                 nums[i] = nums[n - 1];
