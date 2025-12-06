af1a8a3f86e669d10ecd516125935875ced68cc4
nazuratama <nazurawirayudatama7@gmail.com>
2025-12-06 08:58:28 +0700

    problems 32 java with 2 ms

--- problems/Java/32. Longest Valid Parentheses.java
+++ problems/Java/32. Longest Valid Parentheses.java
@@ -18,6 +18,7 @@ class Solution {
         
         l = 0;
         r = 0;
+        
         for (int i = s.length() - 1; i >= 0; i--) {
             if (s.charAt(i) == '(') {
                 l++;
