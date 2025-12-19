10fcff2113ba8fe9a30121264cf3c03c7d236119
nazuratama <nazurawirayudatama7@gmail.com>
2025-12-19 18:32:39 +0700

    problems 110 c++ with 0 ms

---
+++ problems/C++/110. Balanced Binary Tree.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        return dfs(root) != -1;
+    }
+
+    int dfs(TreeNode* node) {
+        if (!node) return 0;
+
+        int l = dfs(node->left);
+        if (l == -1) return -1;
+
+        int r = dfs(node->right);
+        if (r == -1) return -1;
+
+        if (std::abs(l - r) > 1) return -1;
+
+        return 1 + std::max(l, r);
+    }
+};
