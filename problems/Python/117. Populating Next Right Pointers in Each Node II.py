d404925beb1e27a9c15409c68f01e453670fdb0b
nazuratama <nazurawirayudatama7@gmail.com>
2025-12-26 20:16:37 +0700

    problems 117 python with 37 ms

---
+++ problems/Python/117. Populating Next Right Pointers in Each Node II.py
@@ -0,0 +1,32 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return None
+        
+        cur = root
+        while cur:
+            dum = Node(0)
+            tail = dum
+            
+            while cur:
+                if cur.left:
+                    tail.next = cur.left
+                    tail = tail.next
+                if cur.right:
+                    tail.next = cur.right
+                    tail = tail.next
+                cur = cur.next
+                
+            cur = dum.next
+            
+        return root
