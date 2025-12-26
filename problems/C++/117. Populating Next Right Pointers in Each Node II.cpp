1d95d4cc4595adc1cb675bca1cb24e5a940a3390
nazuratama <nazurawirayudatama7@gmail.com>
2025-12-26 20:45:48 +0700

    problems 117 c++ with 6 ms

---
+++ problems/C++/117. Populating Next Right Pointers in Each Node II.cpp
@@ -0,0 +1,45 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if (!root) return nullptr;
+        
+        Node* cur = root;
+        while (cur) {
+            Node dum(0);
+            Node* tail = &dum;
+            
+            while (cur) {
+                if (cur->left) {
+                    tail->next = cur->left;
+                    tail = tail->next;
+                }
+                if (cur->right) {
+                    tail->next = cur->right;
+                    tail = tail->next;
+                }
+                cur = cur->next;
+            }
+            cur = dum.next;
+        }
+        return root;
+    }
+};
+
