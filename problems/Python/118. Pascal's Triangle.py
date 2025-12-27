0c04ee225a595c75f71f496b8238c1717f59f957
nazuratama <nazurawirayudatama7@gmail.com>
2025-12-27 15:46:57 +0700

    problems 118 python with 0 ms

--- problems/Python/118. Pascal's Triangle.py
+++ problems/Python/118. Pascal's Triangle.py
@@ -1,6 +1,7 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         res = []
+        
         for i in range(numRows):
             row = [1] * (i + 1)
             for j in range(1, i):
