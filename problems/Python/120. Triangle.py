c26b2f56dd1c74a650e924f3910f8adbc9d8b1a3
nazuratama <nazurawirayudatama7@gmail.com>
2025-12-29 21:48:24 +0700

    problems 120 python with 0 ms

--- problems/Python/120. Triangle.py
+++ problems/Python/120. Triangle.py
@@ -1,10 +1,10 @@
 class Solution:
-    def minimumTotal(self, tri: List[List[int]]) -> int:
-        dp = tri[-1]
-        n = len(tri)
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        dp = triangle[-1]
+        n = len(triangle)
         
         for i in range(n - 2, -1, -1):
-            for j in range(len(tri[i])):
-                dp[j] = tri[i][j] + min(dp[j], dp[j + 1])
+            for j in range(len(triangle[i])):
+                dp[j] = triangle[i][j] + min(dp[j], dp[j + 1])
                 
         return dp[0]
