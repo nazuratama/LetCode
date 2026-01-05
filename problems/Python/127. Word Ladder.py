31ffce7e7981eb7b850bd7b883b5e9690fa6535d
nazuratama <nazurawirayudatama7@gmail.com>
2026-01-05 23:01:31 +0700

    problems 127 python with 17 ms

---
+++ problems/Python/127. Word Ladder.py
@@ -0,0 +1,32 @@
+class Solution:
+    def ladderLength(self, beg: str, end: str, wl: List[str]) -> int:
+        d = set(wl)
+        if end not in d:
+            return 0
+        
+        s1 = {beg}
+        s2 = {end}
+        stp = 1
+        
+        while s1 and s2:
+            if len(s1) > len(s2):
+                s1, s2 = s2, s1
+            
+            nxt = set()
+            for w in s1:
+                wa = list(w)
+                for i in range(len(wa)):
+                    org = wa[i]
+                    for c in 'abcdefghijklmnopqrstuvwxyz':
+                        wa[i] = c
+                        nw = "".join(wa)
+                        if nw in s2:
+                            return stp + 1
+                        if nw in d:
+                            nxt.add(nw)
+                            d.remove(nw)
+                    wa[i] = org
+            s1 = nxt
+            stp += 1
+            
+        return 0
