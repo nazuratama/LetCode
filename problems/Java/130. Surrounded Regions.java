63c465164b42c867a1a79e75b1b0a09fc71b5fc3
nazuratama <nazurawirayudatama7@gmail.com>
2026-01-08 23:29:04 +0700

    problems 130 java with 1 ms

---
+++ problems/Java/130. Surrounded Regions.java
@@ -0,0 +1,34 @@
+class Solution {
+    int m, n;
+    
+    public void solve(char[][] board) {
+        m = board.length;
+        n = board[0].length;
+        
+        for (int i = 0; i < m; i++) {
+            if (board[i][0] == 'O') dfs(board, i, 0);
+            if (board[i][n-1] == 'O') dfs(board, i, n-1);
+        }
+        
+        for (int j = 0; j < n; j++) {
+            if (board[0][j] == 'O') dfs(board, 0, j);
+            if (board[m-1][j] == 'O') dfs(board, m-1, j);
+        }
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') board[i][j] = 'X';
+                else if (board[i][j] == 'T') board[i][j] = 'O';
+            }
+        }
+    }
+    
+    private void dfs(char[][] board, int r, int c) {
+        if (r < 0 || r >= m || c < 0 || c >= n || board[r][c] != 'O') return;
+        board[r][c] = 'T';
+        dfs(board, r+1, c);
+        dfs(board, r-1, c);
+        dfs(board, r, c+1);
+        dfs(board, r, c-1);
+    }
+}
